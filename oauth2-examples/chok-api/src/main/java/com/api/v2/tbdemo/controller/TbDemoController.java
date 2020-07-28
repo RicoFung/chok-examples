8d7b44fb6dbddb65324a65538ccf3dbab320e81a
zhuojun.feng <zhuojun.feng@moco.com>
2020-07-28 15:07:40 +0800

    实现DEMO【导入】逻辑

--- oauth2-examples/chok-api/src/main/java/com/api/v2/tbdemo/controller/TbDemoController.java
+++ oauth2-examples/chok-api/src/main/java/com/api/v2/tbdemo/controller/TbDemoController.java
@@ -1,5 +1,8 @@
 package com.api.v2.tbdemo.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
@@ -11,27 +14,42 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.api.v2.entity.TbDemo;
 import com.api.v2.tbdemo.dto.TbDemoAddDTO;
 import com.api.v2.tbdemo.dto.TbDemoDelDTO;
 import com.api.v2.tbdemo.dto.TbDemoGetDTO;
+import com.api.v2.tbdemo.dto.TbDemoImpDTO;
 import com.api.v2.tbdemo.dto.TbDemoQueryDTO;
 import com.api.v2.tbdemo.dto.TbDemoUpdDTO;
-import com.api.v2.entity.TbDemo;
 import com.api.v2.tbdemo.service.TbDemoService;
 import com.fasterxml.jackson.core.type.TypeReference;
 
+import chok.common.RestConstants;
 import chok.common.RestResult;
 import chok.devwork.springboot.BaseRestController;
+import chok.util.POIUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 
 @Api(tags = "v2-TbDemo")
 @RestController(value = "v2TbDemoController")
 @RequestMapping("/api/v2/tbdemo")
 public class TbDemoController extends BaseRestController<TbDemo>
 {
+	// --------------------------------------------------------------------------------------- //
+	// value: 指定请求的实际地址， 比如 /action/info之类
+	// method： 指定请求的method类型， GET、POST、PUT、DELETE等
+	// consumes： 指定处理请求的提交内容类型（Content-Type），例如application/json, text/html;
+	// produces: 指定返回的内容类型，仅当request请求头中的(Accept)类型中包含该指定类型才返回
+	// params： 指定request中必须包含某些参数值是，才让该方法处理
+	// headers： 指定request中必须包含某些指定的header值，才能让该方法处理请求
+	// --------------------------------------------------------------------------------------- //
+	
 	private final Logger log = LoggerFactory.getLogger(getClass());
 
 	@Autowired
@@ -52,6 +70,7 @@ public class TbDemoController extends BaseRestController<TbDemo>
 			if (validResult.hasErrors()) 
 			{
 				restResult.setSuccess(false);
+				restResult.setCode(RestConstants.ERROR_CODE1);
 				restResult.setMsg(getValidMsgs(validResult));
 				return restResult;
 			}
@@ -63,6 +82,7 @@ public class TbDemoController extends BaseRestController<TbDemo>
 		{
 			log.error("<== Exception：{}", e);
 			restResult.setSuccess(false);
+			restResult.setCode(RestConstants.ERROR_CODE1);
 			restResult.setMsg(e.getMessage());
 		}
 		return restResult;
@@ -82,6 +102,7 @@ public class TbDemoController extends BaseRestController<TbDemo>
 			if (validResult.hasErrors()) 
 			{
 				restResult.setSuccess(false);
+				restResult.setCode(RestConstants.ERROR_CODE1);
 				restResult.setMsg(getValidMsgs(validResult));
 				return restResult;
 			}
@@ -92,6 +113,7 @@ public class TbDemoController extends BaseRestController<TbDemo>
 		{
 			log.error("<== Exception：{}", e);
 			restResult.setSuccess(false);
+			restResult.setCode(RestConstants.ERROR_CODE1);
 			restResult.setMsg(e.getMessage());
 		}
 		return restResult;
@@ -111,6 +133,7 @@ public class TbDemoController extends BaseRestController<TbDemo>
 			if (validResult.hasErrors()) 
 			{
 				restResult.setSuccess(false);
+				restResult.setCode(RestConstants.ERROR_CODE1);
 				restResult.setMsg(getValidMsgs(validResult));
 				return restResult;
 			}
@@ -141,6 +164,7 @@ public class TbDemoController extends BaseRestController<TbDemo>
 			if (validResult.hasErrors()) 
 			{
 				restResult.setSuccess(false);
+				restResult.setCode(RestConstants.ERROR_CODE1);
 				restResult.setMsg(getValidMsgs(validResult));
 				return restResult;
 			}
@@ -154,6 +178,7 @@ public class TbDemoController extends BaseRestController<TbDemo>
 		{
 			log.error("<== Exception：{}", e);
 			restResult.setSuccess(false);
+			restResult.setCode(RestConstants.ERROR_CODE1);
 			restResult.setMsg(e.getMessage());
 		}
 		return restResult;
@@ -178,8 +203,52 @@ public class TbDemoController extends BaseRestController<TbDemo>
 		{
 			log.error("<== Exception：{}", e);
 			restResult.setSuccess(false);
+			restResult.setCode(RestConstants.ERROR_CODE1);
 			restResult.setMsg(e.getMessage());
 		}
 		return restResult;
 	}
+	
+	@ApiOperation("上传")
+	@RequestMapping(value = "/imp", method = RequestMethod.POST, consumes = "multipart/*", headers = "content-type=multipart/form-data")
+	public RestResult imp(@RequestParam("file") MultipartFile file, @ApiParam(name="json", value = "{\"param1\":\"1\",\"param2\":\"2\"}") @RequestParam("json") String json)
+	{
+		restResult = new RestResult();
+		try
+		{
+			if (log.isDebugEnabled())
+			{
+				log.debug("==> requestDto：filename={}, json={}", file.getOriginalFilename(), json);
+			}
+			restResult = validImportBefore(file, json, TbDemoImpDTO.class);
+			if (!restResult.isSuccess())
+			{
+				return restResult;
+			}
+//			String tcUuid = UUID.randomUUID().toString();
+			List<TbDemo> impList = new ArrayList<TbDemo>();
+			List<String[]> excelList = POIUtil.readExcel(file, 1);
+			// Excel 2 List<T>
+			for(String[] excelRow : excelList)
+			{
+				TbDemo tbDemo = new TbDemo();
+				Map<String, Object> m = new HashMap<String, Object>();
+				m.put("tcCode", excelRow[0]);
+				m.put("tcName", excelRow[1]);
+				org.apache.commons.beanutils.BeanUtils.populate(tbDemo, m);
+				impList.add(tbDemo);
+			}
+			// 分批写入，每批100条
+			service.addBatch(impList, 100);
+		}
+		catch (Exception e)
+		{
+			log.error("<== Exception：{}", e);
+			restResult.setSuccess(false);
+			restResult.setCode(RestConstants.ERROR_CODE1);
+			restResult.setMsg(e.getMessage());
+		}
+		return restResult;
+	}
+	
 }
